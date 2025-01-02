ee254c9d96a50347e621f478ce0b581faac21ecf
buyakaa13 <buyakaa95572377@gmail.com>
2025-01-02 15:19:39 -0600

    fixed Item

--- src/main/java/org/example/coffeeshop/Entity/Item.java
+++ src/main/java/org/example/coffeeshop/Entity/Item.java
@@ -55,11 +55,11 @@ public class Item {
         return quantity;
     }
 
-    public List<OrderItem> getOrderItems() {
-        return orderItems;
-    }
-
-    public void setOrderItems(List<OrderItem> orderItems) {
-        this.orderItems = orderItems;
-    }
+//    public List<OrderItem> getOrderItems() {
+//        return orderItems;
+//    }
+//
+//    public void setOrderItems(List<OrderItem> orderItems) {
+//        this.orderItems = orderItems;
+//    }
 }
